95c77c4500a00e561e4ed3e489219cfc5b7f8b14
Nitin <nsinghboss02@gmail.com>
2022-01-12 11:23:04 +0530

    Initial commit

---
+++ src/com/Recursion/Main.java
@@ -0,0 +1,20 @@
+package com.Recursion;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	// java program to reverse words in a given String
+        String str = "i.would.like.to.have.some.tea";
+        String st = reverse(str);
+        System.out.println(st);
+
+    }
+    public static String reverse(String str){
+        String s[] = str.split("\\.");
+        String rev = "";
+        for(int i=s.length-1;i>0;i--){
+            rev = rev+s[i]+".";
+        }
+        return rev +s[0];
+    }
+}
